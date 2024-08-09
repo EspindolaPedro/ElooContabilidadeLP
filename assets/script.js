c4f4ecc1bb019081642861225c4dad5ff6747089
PedroPaulo <131112587+EspindolaPedro@users.noreply.github.com>
2024-08-09 12:01:09 -0400

    Slide desenvolvido

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,64 @@ const btnMobile = document.getElementById("btn-mobile"); function toggleMenu(e) 
       btnMobile.setAttribute('aria-expanded', 'false');
     });
   });
+
+
+
+list = document.querySelector('.swipe-list');
+
+let imgList = [
+  {
+    id:1,
+    img:'assets/depoimentos/dep1.webp'
+  },
+  {
+    id:2,
+    img:'assets/depoimentos/dep1.webp'
+  },
+  {
+    id:3,
+    img:'assets/depoimentos/dep1.webp'
+  },
+  {
+    id:4,
+    img:'assets/depoimentos/dep1.webp'
+  },
+  {
+    id:5,
+    img:'assets/depoimentos/dep1.webp'
+  },
+  {
+    id:6,
+    img:'assets/depoimentos/dep1.webp'
+  },
+  {
+    id:7,
+    img:'assets/depoimentos/dep1.webp'
+  },
+  {
+    id:8,
+    img:'assets/depoimentos/dep1.webp'
+  },
+  {
+    id:9,
+    img:'assets/depoimentos/dep1.webp'
+  },
+  {
+    id:10,
+    img:'assets/depoimentos/dep1.webp'
+  },
+]
+
+const initlist = () => {
+  imgList.forEach((value,key) => {
+    let newImg = document.createElement('img');
+    newImg.src = value.img;
+    let listItem = document.createElement('li');
+    listItem.classList.add('list-swipe');
+    listItem.appendChild(newImg);
+    document.querySelector('.slide').appendChild(listItem)
+  })
+    
+}
+
+initlist();
